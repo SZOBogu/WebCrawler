13c5d1a5175e89d1171e4745c9ccc82353f97d83
Bogu <adambozek1@gmail.com>
2020-02-06 17:22:42 +0100

    LinkFetcher debugged and fixed.

--- venv/src/LinkFetcher.py
+++ venv/src/LinkFetcher.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from Filter import Filter
 import csv
 from pathlib import Path
+import os
 
 
 class LinkFetcher:
@@ -22,27 +23,25 @@ class LinkFetcher:
 
     def getLinks(self, linksFilePath, i):
         sett = set()
-        setemp = set()
         linksFilePath = Path(linksFilePath)
         linksFilePath.touch(exist_ok=True)
 
-        with open(linksFilePath) as file:
-            url = file.read().split('\n')[i]
-            source = "https://pl.wikipedia.org/" + url
-            data = requests.get(source).text
-            soup = BeautifulSoup(data, 'lxml')
-            # for line in soup.find_all('p'):
-            #     line = line.get_text()
-            #     line = re.sub('\W+', ' ', line)
-            #     line = line.lower()
+        if(os.stat(linksFilePath).st_size != 0):
+            with open(linksFilePath) as file:
+                url = file.read().split('\n')[i]
+                source = "https://pl.wikipedia.org/" + url
+                data = requests.get(source).text
+                soup = BeautifulSoup(data, 'lxml')
 
-            filter = Filter()
-            sett = filter.filter(soup.find_all('a'))
+                filter = Filter()
+                sett = filter.filter(soup.find_all('a'))
 
-            with open(linksFilePath, 'a') as file:
-                writer = csv.writer(file, delimiter=' ')
-                for link in setemp:
-                    print(link)
-                    writer.writerow([link])
-            return sett
+                with open(linksFilePath, 'a') as file:
+                    writer = csv.writer(file, delimiter=' ')
+                    for link in sett:
+                        print(link)
+                        writer.writerow([link])
+                return sett
+        else:
+            print('file empty')
         return sett
