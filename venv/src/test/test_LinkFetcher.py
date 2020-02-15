ef9bfe7d88c7babffada63a6516f48f171d03fbf
Bogu <adambozek1@gmail.com>
2020-02-15 01:09:32 +0100

    testLink file adjusted for testing. LinkCount() test fixed.

--- venv/src/test/test_LinkFetcher.py
+++ venv/src/test/test_LinkFetcher.py
@@ -8,10 +8,24 @@ class MyTestCase(unittest.TestCase):
         self.fetcher = LinkFetcher()
 
     def test_linkCount(self):
-        self.assertEqual(3, self.fetcher.linkCount('testLink'))
+        self.assertEqual(14, self.fetcher.linkCount('testLink'))
 
     def test_getLinks(self):
-        sett = {'na jeziorze', 'wielka burza', 'jezus ze mna w lodzi jest'}
+        sett = {"/wiki/Suomenlinna",
+                "/wiki/Plac_Targowy_w_Helsinkach",
+                "/wiki/Komisja_Standaryzacji_Nazw_Geograficznych_poza_Granicami_Rzeczypospolitej_Polskiej",
+                "/wiki/Helsinki",
+                "/wiki/Morze_Ba%C5%82tyckie",
+                "/wiki/Suomenlinna",
+                "/wiki/Katedra",
+                "/wiki/Pa%C5%82ac_Prezydencki_w_Helsinkach",
+                "/wiki/Finlandia",
+                "/wiki/Szwecja",
+                "/wiki/Urz%C4%85d_miejski",
+                "/wiki/Wikimedia_Commons",
+                "/wiki/Plac_Targowy_w_Helsinkach",
+                "/wiki/Ambasada",
+                "/wiki/Esplanadi",}
         self.assertEqual(sett, self.fetcher.getLinks('testLink', 1))
 
 if __name__ == '__main__':
